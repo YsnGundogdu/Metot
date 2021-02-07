687a0c414c3767b2fabc165e32d05bfda94138f7
yasin gündoğdu <ysn_gundogdu@hotmail.com>
2021-02-07 19:35:21 +0300

    Add project files.

---
+++ Metot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine("Urun Adi: " + urun.Adi);
+                Console.WriteLine("Urun Fiyati: " + urun.Fiyati);
+                Console.WriteLine("Urun Aciklamasi: " + urun.Aciklama);
+                Console.WriteLine("------------------------------------------\n");
+            }
+
+            Console.WriteLine("---------------Metotlar---------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12);
+
+        }
+    }
+}
